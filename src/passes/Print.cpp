dd5f0437c8caf55ec71fd9dd0603860cc814c832
Alon Zakai <azakai@google.com>
2024-05-08 16:41:23 -0700

    WEIRD.2

--- src/passes/Print.cpp
+++ src/passes/Print.cpp
@@ -3023,7 +3023,7 @@ void PrintSExpression::visitDefinedFunction(Function* curr) {
     assert(controlFlowDepth == 0);
   } else {
     // Print the stack IR.
-    printStackIR(curr->stackIR.get(), *this);
+    printStackIR(&*curr->stackIR, *this);
   }
   if (currFunction->epilogLocation.size()) {
     // Print last debug location: mix of decIndent and printDebugLocation
@@ -3514,8 +3514,9 @@ static std::ostream& printStackIR(StackIR* ir, PrintSExpression& printer) {
   int controlFlowDepth = 0;
   // Stack to track indices of catches within a try
   SmallVector<Index, 4> catchIndexStack;
+  auto& insts = ir->insts;
   for (Index i = 0; i < (*ir).size(); i++) {
-    auto* inst = (*ir)[i];
+    auto* inst = insts[i];
     if (!inst) {
       continue;
     }
