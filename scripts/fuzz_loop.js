5997151ac34c4f53490d92de268b6e4814fc5c36
Alon Zakai <azakai@google.com>
2024-10-03 14:47:57 -0700

    work

--- scripts/fuzz_loop.js
+++ scripts/fuzz_loop.js
@@ -1,4 +1,19 @@
+
+// Fuzz loop: generates and fuzzes wasms in an infinite loop or until we find a
+// problem.
+//
+// Usage:
+//         d8 scripts/fuzz_loop.js [PATH../]binaryen_wasm.js
+//
 // This should be run from the toplevel of the binaryen repo, as the paths are
 // relative to there.
-console.log('loading fuzz shell');
+
+// Read the argument, and clear it, as fuzz_shell should not see any argument.
+const binaryen_wasm_js_path = arguments[0];
+arguments.length = 0;
+
+console.log(`fuzz_loop: loading binaryen_wasm.js ({binaryen_wasm_js_path})`);
+load(binaryen_wasm_js_path);
+
+console.log('fuzz_loop: loading fuzz shell');
 load('scripts/fuzz_shell.js');
