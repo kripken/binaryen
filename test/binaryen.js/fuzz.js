0fc9dca3d2e349f7bf4ba022d1e11da113de64d7
Alon Zakai <azakai@google.com>
2024-10-03 13:52:25 -0700

    work

---
+++ test/binaryen.js/fuzz.js
@@ -0,0 +1,31 @@
+// Some "random" data.
+const NUM = 2048;
+const bytes = new Uint8Array(NUM);
+for (let i = 0; i < NUM; i++) {
+  bytes[i] = (i * i) & 0xff;
+}
+
+// Build with and without allowOOB.
+const sizes = [];
+for (let oob = 0; oob < 2; oob++) {
+  // Create an empty module and add fuzz.
+  const module = new binaryen.Module();
+  module.addFuzz(bytes, NUM, oob);
+
+  // The output must be valid.
+  assert(module.validate());
+
+  // The size must be reasonable.
+  const size = binaryen.emitText(module).length;
+  assert(size >= 20);
+  sizes[oob] = size;
+
+console.log(binaryen.emitText(module));
+throw 5;
+
+  module.dispose();
+}
+
+// Test that the OOB mode had an effect.
+assert(sizes[0] != sizes[1]);
+
