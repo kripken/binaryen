9f389af6dcca77d28468c0db034594c8f5eee239
Alon Zakai <azakai@google.com>
2024-10-03 15:53:13 -0700

    works in d8

--- scripts/fuzz_loop.js
+++ scripts/fuzz_loop.js
@@ -3,17 +3,21 @@
 // problem.
 //
 // Usage:
-//         d8 scripts/fuzz_loop.js [PATH../]binaryen_wasm.js
+//         d8 scripts/fuzz_loop.js [ABSOLUTE_PATH../]binaryen_wasm.js
 //
-// This should be run from the toplevel of the binaryen repo, as the paths are
-// relative to there.
+// The path to binaryen_wasm.js must be absolute.
 
 // Read the argument, and clear it, as fuzz_shell should not see any argument.
 const binaryen_wasm_js_path = arguments[0];
 arguments.length = 0;
 
-console.log(`fuzz_loop: loading binaryen_wasm.js ({binaryen_wasm_js_path})`);
-load(binaryen_wasm_js_path);
-
 console.log('fuzz_loop: loading fuzz shell');
 load('scripts/fuzz_shell.js');
+
+console.log(`fuzz_loop: loading binaryen_wasm.js ({binaryen_wasm_js_path})`);
+import(binaryen_wasm_js_path).then((imported) => {
+  return imported.default();
+}).then((binaryen) => {
+  console.log('fuzz loop: fuzzing forever');
+});
+
