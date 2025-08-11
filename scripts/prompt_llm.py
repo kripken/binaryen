4ff7e45e3f223e26f988f97427b2072ae8c30583
Alon Zakai <azakai@google.com>
2025-08-11 16:16:25 -0700

    work

--- scripts/prompt_llm.py
+++ scripts/prompt_llm.py
@@ -47,16 +47,50 @@ def do_prompt(prompt):
     print(response.text)
 
 
+# Files we always want to include, for context.
+def get_core_files():
+    return [
+        os.path.join(src_dir, 'wasm-types.h'),
+        os.path.join(src_dir, 'literal.h'),
+        os.path.join(src_dir, 'wasm.h'),
+        os.path.join(src_dir, 'wasm-traversal.h'),
+        os.path.join(src_dir, 'pass.h'),
+        os.path.join(src_dir, 'ir', 'effects.h'),
+    ]
+
+
+# Given C++ code, find the headers mentioned there.
+def get_headers_used_by(code):
+    pattern = re.compile(r'^#include\s*"([^"]+)"', re.MULTILINE)
+    return pattern.findall(content)
+
+
+# Given the code of a Binaryen pass, find the commandline flag to use it.
+def get_commandline_pass_name(code):
+    # The pass creates itself using something like
+    #
+    #   Pass* createFoo() { ..
+    #
+    # Using that function name, we can look in pass.cpp to find the commandline
+    # argument.
+    pattern = re.compile(r'^Pass[*] (\w+)[(]', re.MULTILINE)
+    creators = pattern.findall(content)
+    
+
+
 if __name__ == "__main__":
     cmd = sys.argv[1]
     arg = sys.argv[2]
 
     if cmd == 'bugfind':
+        main = arg
+        code = open(filename).read()
+
         prompt = f'''
 You are an expert in compilers. Please look through the attached code and
 try to find bugs in it.
 
-The main source file I would like you to focus on is {arg}. I will provide other
+The main source file I would like you to focus on is {main}. I will provide other
 files too, for context, and if you happen to find a bug there, please report it.
 
 Report only one bug. Try to be sure that it is a bug, or at least that it is
@@ -69,22 +103,20 @@ If you do not find bugs but do find missing corner cases in the tests, that can
 be useful as well, and please mention that too.
 '''
 
-        # Files to bundle.
-        files = set([
-            # The main file.
-            arg,
-            # Several important core files that we always want.
-            os.path.join(src_dir, 'wasm-types.h'),
-            os.path.join(src_dir, 'literal.h'),
-            os.path.join(src_dir, 'wasm.h'),
-            os.path.join(src_dir, 'wasm-traversal.h'),
-            os.path.join(src_dir, 'pass.h'),
-            os.path.join(src_dir, 'ir', 'effects.h'),
-        ])
+        # Files to bundle. Start with core files, plus the main one.
+        files = get_core_files()
+        files.add(main)
 
-        # Add all headers that the main file refers to
+        # Add all headers that the main file refers to.
+        files.update(get_headers_used_by(code))
 
-        subprocess.check_call(['python3', os.path.join(script_dir, 'bundle_llm.py', arg])
+        # Find the commandline name of the pass, and find all test files with
+        # that name in them.
+        files.update(get_tests_with_name(get_commandline_pass_name(code)))
+
+        print(files)
+        1/0
+        subprocess.check_call(['python3', os.path.join(script_dir, 'bundle_llm.py'] + files)
         do_prompt(prompt)
     else:
         print('invalid command')
