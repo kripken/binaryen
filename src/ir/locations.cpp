2b29bd4a6a8555bb928ddf61bdb7fa37d7eaed71
Alon Zakai <azakai@google.com>
2025-09-16 16:39:37 -0700

    work

--- src/ir/locations.cpp
+++ src/ir/locations.cpp
@@ -23,6 +23,9 @@ namespace wasm::Locations {
 
 Type getType(const Location& location, Module& wasm) {
   if (auto* loc = std::get_if<wasm::ExpressionLocation>(&location)) {
+    if (loc->expr->type.size() == 0) {
+      return loc->expr->type;
+    }
     return loc->expr->type[loc->tupleIndex];
   } else if (auto* loc = std::get_if<wasm::DataLocation>(&location)) {
     return GCTypeUtils::getField(loc->type, loc->index)->type;
