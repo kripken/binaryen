f25833515979975b48873f3255936a8eea883d23
Alon Zakai <azakai@google.com>
2026-07-24 16:16:42 -0700

    work

--- src/ir/constraint.cpp
+++ src/ir/constraint.cpp
@@ -236,18 +236,18 @@ Result AndedConstraintSet::proves(const AndedConstraintSet& other) const {
 namespace {
 
 // Do an AND on a pair of constraints, looking for a way to fuse them together
-// into a list of constraints that represents them both, while assuming the
+// into a single constraint that represents them both, while assuming the
 // constraints have an equal term. If we fail, return nullopt.
-std::optional<ConstraintVector> fusedApproximateAndTermEqualPair(
+std::optional<Constraint> fusedApproximateAndTermEqualPair(
   const Abstract::Op aOp, const Abstract::Op bOp, const Term& term) {
   using namespace Abstract;
 
   // x < C && x <= C  ===  x < C
   if (aOp == LtS && bOp == LeS) {
-    return ConstraintVector{Constraint{LtS, term}};
+    return Constraint{LtS, term};
   }
   if (aOp == LtU && bOp == LeU) {
-    return ConstraintVector{Constraint{LtU, term}};
+    return Constraint{LtU, term};
   }
 
   // TODO: all the rest
@@ -255,10 +255,16 @@ std::optional<ConstraintVector> fusedApproximateAndTermEqualPair(
   return {};
 }
 
+// Handle Incremented/Incrementing in AND operations.
+std::optional<ConstraintVector> approximateAndIncrement(const Constraint& a,
+                                                        const Constraint& b) {
+  
+}
+
 // Do an AND on a pair of constraints, looking for a way to fuse them together
 // into a list of constraints that represents them both. If we fail, return
 // nullopt.
-std::optional<ConstraintVector> fusedApproximateAndPair(const Constraint& a,
+std::optional<ConstraintVector> approximateAndPair(const Constraint& a,
                                                   const Constraint& b,
                                                   bool recursing = false) {
   // If a proves b is true, all we need is a (e.g. { x == 5 && x > 0 } => x == 5
@@ -268,13 +274,17 @@ std::optional<ConstraintVector> fusedApproximateAndPair(const Constraint& a,
 
   if (a.term == b.term) {
     if (auto result = fusedApproximateAndTermEqualPair(a.op, b.op, a.term)) {
-      return result;
+      return ConstraintVector{*result};
     }
+  }
+
+  if (auto result = approximateAndIncrement(a, b)) {
+    return result;
   }
 
   if (!recursing) {
     // The flipped form may be recognized.
-    return fusedApproximateAndPair(b, a, true);
+    return approximateAndPair(b, a, true);
   }
 
   return {};
@@ -300,7 +310,7 @@ void AndedConstraintSet::approximateAnd(const Constraint& c) {
 
   for (auto& existing : *this) {
     // Some ANDed constraints fuse together into a new constraint.
-    if (auto fused = fusedApproximateAndPair(existing, c)) {
+    if (auto fused = approximateAndPair(existing, c)) {
       // Atm no pattern returns nothing, so we do not need to handle that case.
       assert(!fused->empty());
 
@@ -584,7 +594,7 @@ void BasicBlockConstraintMap::set(Index index, Expression* value) {
     return;
   }
 
-  // Special handling of certain binary operations.
+  // Special handling for Incrementing/Incremented.
   {
     // x = y + 1
     Index y;
