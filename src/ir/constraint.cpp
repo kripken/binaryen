a812dc89b32e7f130bd0016bbc46a7e9c8bc71b5
Alon Zakai <azakai@google.com>
2026-07-24 13:41:16 -0700

    work

--- src/ir/constraint.cpp
+++ src/ir/constraint.cpp
@@ -87,7 +87,9 @@ namespace wasm::constraint {
 // One way of looking at Incremented/Incrementing is as abstract operations with
 // specific semantics defined over them. Another perspective is to see them as
 // sort of like infinity or episilon in calculus: when proving limits, those are
-// not actual values, but represent a *process* that unfolds.
+// not actual values, but represent a *process* that unfolds (i.e. f(infinity)
+// does not mean "calculate f on this one value" but rather "calculate f on a
+// sequence of values and see what happens").
 const auto Incrementing = Abstract::User1;
 const auto Incremented = Abstract::User2;
 
